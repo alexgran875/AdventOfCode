782b6d43a6085c65d1cd0a8af85ac51f917447b1
alexeygorskiy <alexey.gorskiy@gmail.com>
2021-12-18 21:29:00 +0100

    18 good progress

---
+++ day18.py
@@ -0,0 +1,65 @@
+import numpy
+import copy
+import math
+import numpy as np
+from numpy.core.fromnumeric import shape
+from numpy.core.numeric import Inf, Infinity
+import ast
+
+with open('input.txt') as f:
+    input = f.readlines()
+input = list(map(lambda i: i.replace("\n",""),input))
+input_listed = list(map(ast.literal_eval,input))
+
+def add_snailfish_numbers(number1, number2):
+    return [number1, number2]
+
+def recursive(number):
+    if not isinstance(number,int):
+        for i in range(len(number)):
+            recursive(number[i])
+    else:
+        x = 5
+
+def send_explosion_recusrively(recursed_number,explosion):
+
+
+
+
+    if not isinstance(recursed_number,int):
+        for i in range(len(recursed_number)):
+            recursive(recursed_number[i])
+    else:
+        x = 5
+
+
+def explode(whole_number,recursed_number,depth):
+    if depth == 4 and not isinstance(recursed_number,int) and len(recursed_number) == 2:
+        return recursed_number
+    if not isinstance(recursed_number,int):
+        for i in range(len(recursed_number)):
+            explosion = explode(whole_number,recursed_number[i],depth+1)
+            if explosion is None:
+                continue
+            if explosion[0] and explosion[1]:
+                recursed_number[i] = 0
+            if explosion[1] and i+1 < len(recursed_number):
+                if isinstance(recursed_number[i+1],int):
+                    recursed_number[i+1] += explosion[1]
+                    explosion[1] = 0
+                else:
+                    pass
+            if explosion[0] and i-1 >= 0:
+                if isinstance(recursed_number[i-1],int):    
+                    recursed_number[i-1] += explosion[0]
+                    explosion[0] = 0
+                else:
+                    pass
+            return explosion
+
+#temp = add_snailfish_numbers(input_listed[0],input_listed[1])
+#recursive(input_listed)
+explode(input_listed,input_listed,-1)
+temp = str(input_listed)
+
+x = 5
