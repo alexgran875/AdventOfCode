c855a1fcd304c51cdcdd0d79feede2ed836b20ff
alexeygorskiy <alexey.gorskiy@gmail.com>
2021-12-23 21:53:18 +0100

    part1 day19 complete

--- day19.py
+++ day19.py
@@ -43,32 +43,99 @@ def find_overlapping_beacons(dist1, dist2):
             duplicates = np.intersect1d(dist_wrt_beacon1,dist_wrt_beacon2)
             if len(duplicates) >= 12:
                 # will always at least match the zero distance to itself, hence 13 instead of 12
-                return i,j
-    return None,None
+                duplicates = duplicates[duplicates!=0]
+                distance1_idx = np.where(np.in1d(dist_wrt_beacon1, duplicates))[0]
+                distance2_idx = np.where(np.in1d(dist_wrt_beacon2, duplicates))[0]
 
-def get_transform(origin, distance_to):
+                distances1 = np.argsort(dist_wrt_beacon1[distance1_idx])
+                distance1_idx = distance1_idx[distances1]
 
-    n_matches = len(np.intersect1d(origin[:,0],distance_to[:,0]))
-    n_matches += len(np.intersect1d(origin[:,1],distance_to[:,1]))
-    n_matches += len(np.intersect1d(origin[:,2],distance_to[:,2]))
-    n_matches /= 3
-    if n_matches == 12:
-        return
+                distances2 = np.argsort(dist_wrt_beacon2[distance2_idx])
+                distance2_idx = distance2_idx[distances2]
 
-    x = 5
+                return i,j,distance1_idx,distance2_idx
+    return None,None,None,None
+
+def get_transform(origin, other):
+    
+    origin_mat = copy.deepcopy(origin)
+    origin_mat = np.array(origin_mat)
+    other_mat = copy.deepcopy(other)
+    other_mat = np.array(other)
+
+    combinations = [[0,1,2],[0,2,1],[1,0,2],[2,0,1],[1,2,0],[2,1,0]]
+
+    for iBeacon in range(len(origin)):
+        origin_vec = origin_mat[iBeacon,:]
+        other_vec = other_mat[iBeacon,:]
+
+        i = 0
+        for x in range(-1,2):
+            for y in range(-1,2):
+                for z in range(-1,2):
+                    if (x == 0 or y == 0 or z == 0):
+                        continue
+
+                    rotated = other_vec * [x,y,z]
+                    for placement in combinations:
+                        flipped = np.array([ rotated[placement[0]], rotated[placement[1]], rotated[placement[2]] ])
+                        if (flipped == origin_vec).all():
+
+                            all_others_flipped = copy.deepcopy(other_mat)
+                            n_matches = 0
+                            for i in range(other_mat.shape[0]):
+                                all_others_flipped[i,:] = all_others_flipped[i,:] * [x,y,z]
+                                replacement = np.array([all_others_flipped[i,placement[0]], all_others_flipped[i,placement[1]], all_others_flipped[i,placement[2]]])
+                                all_others_flipped[i,:] = replacement
+                                if (all_others_flipped[i,:] == origin_mat[i,:]).all():
+                                    n_matches += 1
+
+                            if n_matches == len(origin):
+                                return [x,y,z], placement
+
+    raise Exception("No possible transform!")
+
+def apply_transformation(mult, placement, offset, beacons):
+    transformed_beacons = copy.deepcopy(beacons)
+
+    for i in range(beacons.shape[0]):
+        transformed_beacons[i,:] = beacons[i,:] * mult
+        flipped = np.array([ transformed_beacons[i,placement[0]], transformed_beacons[i,placement[1]], transformed_beacons[i,placement[2]] ])
+        transformed_beacons[i,:] = flipped
+        transformed_beacons[i,:] += offset
+    
+    return transformed_beacons
+
+
 
 origo = copy.deepcopy(sensor_readings[0])
-origo_distances = get_distances(origo, origo)
-for sensor in range(1, len(sensor_readings)):
-    new_sensor_distances = get_distances(sensor_readings[sensor], sensor_readings[sensor])
-    beacon_idx_1, beacon_idx_2 = find_overlapping_beacons(origo_distances,new_sensor_distances)
-    if beacon_idx_1 is not None:
-        dist_vec_wrt_beacon1 = get_dist_vectors(origo[beacon_idx_1], origo)
-        dist_vec_wrt_beacon2 = get_dist_vectors(sensor_readings[sensor][beacon_idx_2], sensor_readings[sensor])
-        get_transform(dist_vec_wrt_beacon1,dist_vec_wrt_beacon2)
+sensor_readings.pop(0)
+while len(sensor_readings) > 0:
+    for sensor in range(len(sensor_readings)):
+        origo_distances = get_distances(origo, origo)
+        new_sensor_distances = get_distances(sensor_readings[sensor], sensor_readings[sensor])
+        matching_beacon_idx1, matching_beacon_idx2, matching_beacon1, matching_beacon2 = find_overlapping_beacons(origo_distances,new_sensor_distances)
+        if matching_beacon_idx1 is not None:
+            dist_vec_wrt_beacon1 = get_dist_vectors(origo[matching_beacon_idx1], origo)
+            dist_vec_wrt_beacon2 = get_dist_vectors(sensor_readings[sensor][matching_beacon_idx2], sensor_readings[sensor])
 
-    x = 5
+            mult, placement = get_transform(dist_vec_wrt_beacon1[matching_beacon1], dist_vec_wrt_beacon2[matching_beacon2])
+            origin_beacon = origo[matching_beacon_idx1]
+            other_beacon = sensor_readings[sensor][matching_beacon_idx2]
+            other_beacon *= mult
+            other_flipped = np.array([ other_beacon[placement[0]], other_beacon[placement[1]], other_beacon[placement[2]] ])
+            offset = origin_beacon - other_flipped
 
-x = 5
+            #new_beacons_wrt_origo = apply_transformation(mult, placement, offset, np.array(sensor_readings[sensor])[matching_beacon2])
+            new_beacons_wrt_origo = apply_transformation(mult, placement, offset, np.array(sensor_readings[sensor]))
+            for i in range(new_beacons_wrt_origo.shape[0]):
+                if i in matching_beacon2 or i == matching_beacon_idx2:
+                    # TODO: new_beacons_wrt_origo[matching_beacon_idx2] does not equal np.array(origo)[matching_beacon_idx1] for some reason
+                    continue
+                origo.append(new_beacons_wrt_origo[i,:])
+            sensor_readings.pop(sensor)
+            print(len(sensor_readings))
+            break
 
+print(len(origo))
 
