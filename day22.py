c6fb90148802f2fbeaa47ef3a9176981e263e613
alexeygorskiy <alexey.gorskiy@gmail.com>
2021-12-25 03:41:18 +0100

    fails on 4th

--- day22.py
+++ day22.py
@@ -7,6 +7,29 @@ from numpy.core.numeric import Inf, Infinity
 with open('input.txt') as f:
     input = f.readlines()
 
+def retired_algo(input):
+    on_coords = set()
+    for line in input:
+        x_min = int(line.split("x=")[1].split("..")[0])
+        x_max = int(line.split(",y=")[0].split("..")[1])
+        y_min = int(line.split(",y=")[1].split("..")[0])
+        y_max = int(line.split(",z=")[0].split("..")[-1])
+        z_min = int(line.split(",z=")[-1].replace("\n","").split("..")[0])
+        z_max = int(line.split(",z=")[-1].replace("\n","").split("..")[-1])
+        if line.count("on"):
+            on = True
+        else:
+            on = False
+        for x in range(x_min,x_max+1):
+            for y in range(y_min,y_max+1):
+                for z in range(z_min,z_max+1):
+                    if on:
+                        on_coords.add((x,y,z))
+                    else:
+                        if (x,y,z) in on_coords:
+                            on_coords.remove((x,y,z))  
+    return len(on_coords)
+
 class Cube():
     def __init__(self, x_min, x_max, y_min, y_max, z_min, z_max, on, addition_order) -> None:
         self.x_min = x_min
@@ -17,9 +40,13 @@ class Cube():
         self.z_max = z_max
         self.on = on
         self.addition_order = addition_order
+        self.volume = None
 
     def get_volume(self):
-        return abs(self.x_max-self.x_min)*abs(self.y_max-self.y_min)*abs(self.z_max-self.z_min)
+        if self.volume is not None:
+            return self.volume
+        self.volume = (abs(self.x_max-self.x_min)+1)*(abs(self.y_max-self.y_min)+1)*(abs(self.z_max-self.z_min)+1)
+        return self.volume
 
     def get_overlapping_cube(self, other_cube):
         # TODO: maybe just less than on the comparison?
@@ -49,33 +76,44 @@ class Cube():
     def equal_to(self, other_cube):
         if self.x_min != other_cube.x_min or self.x_max != other_cube.x_max:
             return False
-            
         if self.y_min != other_cube.y_min or self.y_max != other_cube.y_max:
             return False
-
         if self.z_min != other_cube.z_min or self.z_max != other_cube.z_max:
             return False
-
         return True
 
-
     def subtract_cube(self, overlapping_cube):
-        # TODO: make sure it doesn't double count the sides!!!
-        # assert the volume at the end
-        # does not return the overlapping cube
         cubes = []
-        x_pairings = [(min(self.x_min,overlapping_cube.x_min),overlapping_cube.x_min),
-        (min(self.x_max,overlapping_cube.x_max),self.x_max),
+        x_pairings = [(min(self.x_min,overlapping_cube.x_min),overlapping_cube.x_min-1),
+        (min(self.x_max,overlapping_cube.x_max)+1,self.x_max),
         (overlapping_cube.x_min,overlapping_cube.x_max)]
 
-        y_pairings = [(min(self.y_min,overlapping_cube.y_min),overlapping_cube.y_min),
-        (min(self.y_max,overlapping_cube.y_max),self.y_max),
+        y_pairings = [(min(self.y_min,overlapping_cube.y_min),overlapping_cube.y_min-1),
+        (min(self.y_max,overlapping_cube.y_max)+1,self.y_max),
         (overlapping_cube.y_min,overlapping_cube.y_max)]
 
-        z_pairings = [(min(self.z_min,overlapping_cube.z_min),overlapping_cube.z_min),
-        (min(self.z_max,overlapping_cube.z_max),self.z_max),
+        z_pairings = [(min(self.z_min,overlapping_cube.z_min),overlapping_cube.z_min-1),
+        (min(self.z_max,overlapping_cube.z_max)+1,self.z_max),
         (overlapping_cube.z_min,overlapping_cube.z_max)]
 
+        copied_pairings = copy.deepcopy(x_pairings)
+        for x_pair in x_pairings:
+            if x_pair[0] > x_pair[1]:
+                copied_pairings.remove(x_pair)
+        x_pairings = copy.deepcopy(copied_pairings)
+
+        copied_pairings = copy.deepcopy(y_pairings)
+        for y_pair in y_pairings:
+            if y_pair[0] > y_pair[1]:
+                copied_pairings.remove(y_pair)
+        y_pairings = copy.deepcopy(copied_pairings)
+
+        copied_pairings = copy.deepcopy(z_pairings)
+        for z_pair in z_pairings:
+            if z_pair[0] > z_pair[1]:
+                copied_pairings.remove(z_pair)
+        z_pairings = copy.deepcopy(copied_pairings)
+                    
         for x_pair in x_pairings:
             for y_pair in y_pairings:
                 for z_pair in z_pairings:
@@ -83,13 +121,29 @@ class Cube():
                     and y_pair == (overlapping_cube.y_min, overlapping_cube.y_max) \
                     and z_pair == (overlapping_cube.z_min, overlapping_cube.z_max):
                         continue
-                        
-                    cubes.append(Cube(x_pair[0],x_pair[1],y_pair[0],y_pair[1],z_pair[0],z_pair[1],self.on,self.addition_order))
+
+                    cube = Cube(x_pair[0],x_pair[1],y_pair[0],y_pair[1],z_pair[0],z_pair[1],self.on,self.addition_order)
+                    if cube.get_volume() != 0:
+                        cubes.append(cube)
 
         total_volume = overlapping_cube.get_volume()
         for cube in cubes:
             total_volume += cube.get_volume()
 
+        if total_volume != self.get_volume():
+            x = 5
+            for x_pair in x_pairings:
+                for y_pair in y_pairings:
+                    for z_pair in z_pairings:
+                        if x_pair == (overlapping_cube.x_min, overlapping_cube.x_max) \
+                        and y_pair == (overlapping_cube.y_min, overlapping_cube.y_max) \
+                        and z_pair == (overlapping_cube.z_min, overlapping_cube.z_max):
+                            continue
+                            
+                        cube = Cube(x_pair[0],x_pair[1],y_pair[0],y_pair[1],z_pair[0],z_pair[1],self.on,self.addition_order)
+                        if cube.get_volume() != 0:
+                            cubes.append(cube)
+        
         assert total_volume == self.get_volume()
 
         return cubes
@@ -121,6 +175,7 @@ def get_octadran_ids(cube):
 i = 0
 i_max = len(input)
 octadrans = [ [] for _ in range(8) ]
+cubes = []
 for line in input:
     i += 1
     print(f'{i/i_max}')
@@ -141,18 +196,20 @@ for line in input:
     # AIM: cubes only contains cubes with no overlap!
     # 0: x <= 0, y <= 0, z <= 0
     # BINARY
+    """
     octadran_ids = get_octadran_ids(new_cubes[0])
     cubes = set()
     for id in octadran_ids:
         cubes.update(octadrans[id])
         octadrans[id] = []
-
     cubes = list(cubes)
+    """
     no_additional_checking_needed = []
     while len(new_cubes) > 0:
-        # TODO: no point of a for loop for new cubes since there is an outer while loop,
-        # just index new_cubes[0]
         new_cube = new_cubes[-1]
+        if new_cube.get_volume() == 0:
+            new_cubes.remove(new_cube)
+            continue
         has_overlaps = False
         for cube in cubes:
             overlapping_cube = cube.get_overlapping_cube(new_cube)
@@ -181,6 +238,18 @@ for line in input:
 
                 new_cubes.remove(new_cube)
                 new_cubes.extend(part_cubes_2)
+
+                temp = 0
+                temp2 = 0
+                temp3 = 0
+                for tt in no_additional_checking_needed:
+                    temp += tt.get_volume()
+                for tt in new_cubes:
+                    temp2 += tt.get_volume()
+                for tt in cubes:
+                    temp3 += tt.get_volume()
+
+
                 break
                 #new_cubes.extend(part_cubes_1)
                 #new_cubes.append(overlapping_cube)
@@ -202,8 +271,10 @@ for line in input:
                 no_additional_checking_needed.append(new_cube)
             else:
                 x = 5
-            break
 
+
+    cubes.extend(no_additional_checking_needed)
+    """
     for cube in cubes:
         cube_ids = get_octadran_ids(cube)
         for id in cube_ids:
@@ -215,8 +286,9 @@ for line in input:
         for id in cube_ids:
             assert cube.on
             octadrans[id].append(cube)
+    """
 
-
+"""
 final_cubes = set()
 for i in range(len(octadrans)):
     final_cubes.update(octadrans[i])
@@ -225,6 +297,12 @@ n_on = 0
 for cube in final_cubes:
     assert cube.on
     n_on += cube.get_volume()
+"""
+n_on = 0
+for cube in cubes:
+    assert cube.on
+    n_on += cube.get_volume()
 
 print(n_on)
+print(retired_algo(input))
 
