7ee171d24258ceb2a83782e23c8ac78340d53f3a
alexeygorskiy <alexey.gorskiy@gmail.com>
2022-12-01 06:11:59 +0100

    2022: day1

--- 1.py
+++ 1.py
@@ -1,4 +1,17 @@
 from utils import parse_data
+import numpy as np
 data = parse_data()
 
+all_cals = []
+tmp = 0
+for line in data:
+    if line.isdigit():
+        tmp += int(line)
+    if line == "":
+        all_cals.append(tmp)
+        tmp = 0
 
+all_cals = np.array(all_cals, dtype=np.int32)
+all_cals.sort()
+print(all_cals[-1])
+print(np.sum(all_cals[-3:]))
