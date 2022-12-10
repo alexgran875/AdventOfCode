95466289d94fa3003db93e0ddd5d01e709f86b29
alexeygorskiy <alexey.gorskiy@gmail.com>
2022-12-10 08:27:12 +0100

    2022: 10

--- 10.py
+++ 10.py
@@ -18,7 +18,43 @@ groups = group_data_by_separator(lines)
 answer_a = None
 answer_b = None
 ### --- --- --- ###
+x = 1
+n_cycle = 1
+observe_cycles = [i for i in range(20, 221, 40)]
+sig_str_sum = 0
+drawing_board = ""
+for line in lines:
+    op_cycle_length = 0
+    op_delta_x = 0
+    if line.count("noop"):
+        op_cycle_length = 1
+    if line.count("addx"):
+        op_cycle_length = 2
+        op_delta_x = int(get_digits(line))
+        if line.count("-"):
+            op_delta_x *= -1
+    
+    for _ in range(op_cycle_length):
+        index = (n_cycle - 1) % 40  # index = position
+        if abs(index - x) <= 1:
+            drawing_board += "@"
+        else:
+            drawing_board += " "
 
+        if len(drawing_board.replace("\n", "") ) % 40 == 0:
+            drawing_board += "\n" 
+        
+        if n_cycle in observe_cycles:
+            sig_str = n_cycle * x
+            sig_str_sum += sig_str
+
+        n_cycle += 1
+
+    x += op_delta_x
+
+answer_a = sig_str_sum
+print(drawing_board)
+answer_b = "ZGCJZJFL"
 
 ### --- --- --- ###
 submit_a = False
