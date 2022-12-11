e92abe86095721245fb6eb0a826c4542706ebcf2
alexeygorskiy <alexey.gorskiy@gmail.com>
2022-12-11 08:12:41 +0100

    2022: 11

--- 11.py
+++ 11.py
@@ -20,6 +20,72 @@ answer_b = None
 ### --- --- --- ###
 
 
+class Monkey():
+    def __init__(self, monkey_lines, modulo_arg, divisor):
+        test_arg = int(get_digits(monkey_lines[3]))
+        test_true = int(get_digits(monkey_lines[4]))
+        test_false = int(get_digits(monkey_lines[5]))
+        self.test = lambda item: test_true if (
+            item % test_arg == 0) else test_false
+
+        operation_line = monkey_lines[2]
+        operation_arg = get_digits(operation_line)
+
+        if operation_line.count("*"):
+            if operation_arg == "":
+                self.operation = lambda item: (
+                    (item**2) // divisor) % modulo_arg
+            else:
+                self.operation = lambda item: (
+                    (item*int(operation_arg)) // divisor) % modulo_arg
+        elif operation_line.count("+"):
+            if operation_arg == "":
+                self.operation = lambda item: (
+                    (2*item) // divisor) % modulo_arg
+            else:
+                self.operation = lambda item: (
+                    (item+int(operation_arg)) // divisor) % modulo_arg
+
+        self.n_inspections = 0
+
+        self.items = get_digits(monkey_lines[1], True)
+        self.items = list(map(int, self.items))
+
+    def turn(self, monkeys_list):
+        self.n_inspections += len(self.items)
+        self.items = list(map(self.operation, self.items))
+
+        for item in self.items:
+            monkeys_list[self.test(item)].items.append(item)
+
+        self.items = []
+
+
+def simulate(monkey_groups, smallest_common_multiple, divisor, n_rounds):
+    monkeys_list = []
+    for monkey_lines in monkey_groups:
+        monkeys_list.append(
+            Monkey(monkey_lines, smallest_common_multiple, divisor))
+
+    for _ in range(n_rounds):
+        for monkey in monkeys_list:
+            monkey.turn(monkeys_list)
+
+    monkeys_activity = []
+    for monkey in monkeys_list:
+        monkeys_activity.append(monkey.n_inspections)
+    monkeys_activity = sorted(monkeys_activity)
+
+    return monkeys_activity[-1] * monkeys_activity[-2]
+
+
+smallest_common_multiple = 1
+for line in lines:
+    if line.count("divisible"):
+        smallest_common_multiple *= int(get_digits(line))
+
+answer_a = simulate(groups, smallest_common_multiple, 3, 20)
+answer_b = simulate(groups, smallest_common_multiple, 1, 10000)
 ### --- --- --- ###
 submit_a = False
 submit_b = False
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 Maybe the true Christmas was the bugs we found along the way?
 
-# useful things to remember
-- when using RegEx, don't forget to escape special characters  
-- sets are useful for finding intersections/overlaps as well as subsets  
+# Useful Things to Remember
+- When using RegEx, don't forget to escape special characters  
+- Sets are useful for finding intersections/overlaps as well as subsets  
 - Python [start : end : stop] list notation  
-- memoization: cache results of expensive operations  
-- use primitive solutions to test advanced solutions  
-- code snippets for efficient copy-pasting  
+- Memoization: cache results of expensive operations  
+- Use primitive solutions to test advanced solutions  
+- Code snippets for efficient copy-pasting  
+- A number can be stored compactly if you modulo it by the least common multiple (lcm) of all the division operators that will be performed on it  
