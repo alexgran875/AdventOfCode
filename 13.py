8be6af73f184bc8d8b0a202e9c81c2796a852c35
alexeygorskiy <alexey.gorskiy@gmail.com>
2022-12-13 06:59:32 +0100

    2022: 13

--- 13.py
+++ 13.py
@@ -20,6 +20,65 @@ answer_b = None
 ### --- --- --- ###
 
 
+def is_correct_order(left, right):
+    if type(left) == int and type(right) == int:
+        if left < right:
+            return True
+        elif left > right:
+            return False
+        else:
+            return None
+    elif type(left) == list and type(right) == list:
+        for i in range(max(len(left), len(right))):
+            if i == len(right):
+                return False
+            elif i == len(left):
+                return True
+            order_correct = is_correct_order(left[i], right[i])
+            if order_correct != None:
+                return order_correct
+    elif type(left) == int and type(right) == list:
+        return is_correct_order([left], right)
+    elif type(left) == list and type(right) == int:
+        return is_correct_order(left, [right])
+    return None
+
+
+correct_pairs = []
+for i, pair in enumerate(groups):
+    order_correct = is_correct_order(eval(pair[0]), eval(pair[1]))
+    if order_correct == True:
+        correct_pairs.append(i + 1)
+answer_a = sum(correct_pairs)
+
+ordered_packets = []
+remaining_packets = list(filter(lambda line: line != "", lines))
+divider_1 = "[[2]]"
+divider_2 = "[[6]]"
+divider_2_ordered = False
+remaining_packets.extend([divider_1, divider_2])
+while not divider_2_ordered:
+    print(f"Packets to order: {len(remaining_packets)}")
+    for left_line in remaining_packets:
+        left_is_smallest = True
+        for right_line in remaining_packets:
+            if left_line == right_line:
+                continue
+
+            order_correct = is_correct_order(eval(left_line), eval(right_line))
+            if order_correct == False:
+                left_is_smallest = False
+                break
+
+        if left_is_smallest:
+            ordered_packets.append(left_line)
+            del remaining_packets[remaining_packets.index(left_line)]
+            if left_line == divider_2:
+                divider_2_ordered = True
+            break
+
+answer_b = (ordered_packets.index(divider_1) + 1) * \
+    (ordered_packets.index(divider_2) + 1)
 ### --- --- --- ###
 submit_a = False
 submit_b = False
--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Maybe the true Christmas was the bugs we found along the way?
 - Code snippets for efficient copy-pasting  
 - A number can be stored compactly if you modulo it by the least common multiple (lcm) of all the division operators that will be performed on it  
 - For path finding it does not matter if you travel from start to goal or from goal to start   
+- not None in Python evaluates to True  
+
+# AoC Specific
+- Read instructions VERY carefully and double check the code against the instructions VERY carefully. Don't try to simplify prematurely: if it asks to convert it to a list, then do that. Optimization can come later  
+- Beware of indices (start on 0 vs 1)  
+- Raise exceptions in all else cases just in case you forgot to handle it  
